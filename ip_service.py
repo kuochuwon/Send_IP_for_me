3722d820ded2631691814289e3f3fafca0f66ba7
kuochuwon <42232671+kuochuwon@users.noreply.github.com>
2021-08-09 18:02:10 +0800

    remove comment

--- ip_service.py
+++ ip_service.py
@@ -47,9 +47,7 @@ def load_ip():
 def check_ip_and_update_netsh_rule(curr_win_ip, curr_wsl_ip):
     try:
         # TODO Win工作排程器路徑預設都是Windows32
-        # file_path = r"C:\Users\Roy\Documents\GitHub\MyGit_folder\Projects\Send_IP_for_me\ip_memo.txt"
         file_path = Path(Path.cwd(), "ip_memo.txt")
-        # HINT netsh命令必須有系統管理員身分才可執行，已測試若在工作排程器將安全性選項設定以最高權限執行，可使命令生效
 
         with open(file_path, "r") as f:
             raw = f.read()
@@ -57,6 +55,7 @@ def check_ip_and_update_netsh_rule(curr_win_ip, curr_wsl_ip):
             prev_win_ip = raw_list[0]
             prev_wsl_ip = raw_list[1]
 
+        # HINT netsh命令必須有系統管理員身分才可執行，已測試若在工作排程器將安全性選項設定以最高權限執行，可使命令生效
         delete_netsh_command = f"netsh interface portproxy delete v4tov4 listenport=1942 listenaddress={prev_win_ip}"
         add_netsh_command = (f"netsh interface portproxy add v4tov4 listenport=1942 listenaddress={curr_win_ip} "
                              f"connectport=1942 connectaddress={curr_wsl_ip}")
@@ -129,28 +128,8 @@ def get_windows_ip_info(std_out):
     print("找不到Windows IP")
 
 
-# def readdata():
-#     keyword1 = '無線區域網路介面卡 Wi-Fi'
-#     keyword2 = '乙太網路卡 乙太網路'
-
-#     # HINT: ref: https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp
-#     ip_pattern = "((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)(\\.(?!$)|$)){4}$"
-
-#     with open("ipoutput.txt", "r", encoding="utf-8") as r:
-#         output = r.readlines()
-#         for index, lines in enumerate(output):
-#             if lines.startswith(keyword1) or lines.startswith(keyword2):
-#                 ipv4_rawdata = output[index+4]
-#                 break
-#     ipv4_addr = re.search(ip_pattern, ipv4_rawdata).group(0)
-
-
 def init_log():
-    # log_name = Path(r"C:\Users\Roy\Documents\GitHub\MyGit_folder\Projects\Send_IP_for_me", "logs", "ip_info.log")
     log_name = Path(Path.cwd(), "logs", "ip_info.log")
-
-    # print(log_name)
-    # time.sleep(3)
     path = log_name.parent
     if not (path.exists() and path.is_dir()):
         Path.mkdir(path)
